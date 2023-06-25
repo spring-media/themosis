e0837d4c7262dda927c64ba34c37a61af3144e04
Olivier Gorzalka <olivier@amphibee.fr>
2023-06-25 16:53:15 +0200

    🚀 chore(Kernel.php): add middleware to improve security and performance 🚀 chore(Kernel.php): add middleware aliases for convenience 🚀 chore(Kernel.php): update middleware to use Themosis 4.0 🚀 chore(EncryptCookies.php): add middleware to encrypt cookies 🚀 chore(PreventRequestsDuringMaintenance.php): add middleware to prevent requests during maintenance mode 🚀 chore(TrimStrings.php): add middleware to trim request input strings 🚀 chore(TrustHosts.php): add middleware to trust application's host 🚀 chore(TrustProxies.php): add middleware to trust proxies 🚀 chore(ValidateSignature.php): add middleware to validate signature 🚀 chore(composer.json): update PHP version requirement to 8.1 🚀 chore(composer.json): update dependencies to latest versions

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -13,7 +13,15 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
-    protected $middleware = [];
+    protected $middleware = [
+        // \App\Http\Middleware\TrustHosts::class,
+        \App\Http\Middleware\TrustProxies::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
+        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        \Themosis\Core\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrimStrings::class,
+        \Themosis\Core\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ];
 
     /**
      * The application's route middleware groups.
@@ -29,16 +37,44 @@ class Kernel extends HttpKernel
             'wp.headers',
             'wp.bindings',
             'bindings',
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
             'csrf',
             \Themosis\Route\Middleware\WordPressBodyClass::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         ],
         'api' => [
             'throttle:60,1',
             'wp.can:edit_posts',
             'bindings',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+    ];
+
+    /**
+     * The application's middleware aliases.
+     *
+     * Aliases may be used instead of class names to conveniently assign middleware to routes and groups.
+     *
+     * @var array<string, class-string|string>
+     */
+    protected array $middlewareAliases = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+        'precognitive' => \Themosis\Core\Http\Middleware\HandlePrecognitiveRequests::class,
+        'signed' => \App\Http\Middleware\ValidateSignature::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 
     /**
